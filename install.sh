e9277ab75f4a10c84103236451b68b67bb32fddd
dSebster <100853686+dSebster@users.noreply.github.com>
2022-06-24 06:01:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash/
-cp $HOME/vNodes/src/main.sh $HOME/vnodes && chmod +x $HOME/vnodes && sudo mv $HOME/vnodes /usr/local/bin && vnodes
+cp $HOME/vNodes/src/main.sh $HOME/vnodes && chmod +x $HOME/vnodes && sudo mv $HOME/vnodes /usr/local/bin && cd $HOME && vnodes
