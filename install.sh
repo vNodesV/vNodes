ab3de4be213efdb88d7d8fb40c5d2a5e881f188c
dSebster <100853686+dSebster@users.noreply.github.com>
2022-06-25 00:26:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash/
-cp $HOME/vNodes/src/main.sh /usr/local/bin/deploy && 
-chmod +x /usr/local/bin/deploy && 
+sudo cp $HOME/vNodes/src/main.sh /usr/local/bin/deploy && 
+sudo chmod +x /usr/local/bin/deploy && 
+sudo chown $USER: /usr/local/bin/deploy &&
 cd $HOME && 
 echo "Ready to install. "
 read -p "Hit Enter When Ready or CTRL-C to Cancel."
+deploy
